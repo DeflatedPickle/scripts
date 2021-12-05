6447355c3bc51fdc6c662d092a577de59d1f6e6f
DeflatedPickle <deflatedpickle@gmail.com>
2021-12-05 21:38:45 +0000

    Added JSON output

--- fjijf.py
+++ fjijf.py
@@ -1,11 +1,13 @@
 """
-Finds JAR files with an embedded JAR dependency
+Finds Fabric mods with an embedded JAR dependency
 """
-
+import json
 from pathlib import Path
 from zipfile import ZipFile, is_zipfile
 
 d = input("dir > ")
+
+jars = {}
 
 for p in Path(d).rglob("*.jar"):
     # print(p)
@@ -14,11 +16,20 @@ for p in Path(d).rglob("*.jar"):
         with ZipFile(str(p)) as zf:
             # print(zf)
 
+            with zf.open("fabric.mod.json") as j:
+                j.seek(0)
+                prop = json.load(j, strict=False)
+
             l = []
 
-            for n in zf.namelist():
-                if "jars" in n:
-                    l.append(n.split("/")[-1])
+            if "jars" in prop:
+                for j in prop["jars"]:
+                    l.append(j["file"].split("/")[-1])
 
             if l:
                 print(f"'{p.name}' embeds; {', '.join(l)}")
+                jars[p.name] = l
+
+if jars:
+    with open(Path(d) / "output.json", "w+") as f:
+        f.write(json.dumps(jars, indent=4, sort_keys=True))

b64c0010cd0993398949f40528b274af8c7db19d
DeflatedPickle <deflatedpickle@gmail.com>
2021-12-06 23:09:54 +0000

    Made it recursive

--- fjijf.py
+++ fjijf.py
@@ -2,6 +2,7 @@
 Finds Fabric mods with an embedded JAR dependency
 """
 import json
+from io import BytesIO
 from pathlib import Path
 from zipfile import ZipFile, is_zipfile
 
@@ -9,26 +10,36 @@ d = input("dir > ")
 
 jars = {}
 
-for p in Path(d).rglob("*.jar"):
-    # print(p)
 
-    if is_zipfile(str(p)):
-        with ZipFile(str(p)) as zf:
-            # print(zf)
-
+def recurse(f, n, o, t):
+    if is_zipfile(f):
+        with ZipFile(f) as zf:
             with zf.open("fabric.mod.json") as j:
                 j.seek(0)
                 prop = json.load(j, strict=False)
 
-            l = []
+            l = {}
 
             if "jars" in prop:
                 for j in prop["jars"]:
-                    l.append(j["file"].split("/")[-1])
+                    l[j["file"].split("/")[-1]] = recurse(
+                        BytesIO(zf.read(j["file"])), j["file"].split("/")[-1],
+                        o, False,
+                    )
 
             if l:
-                print(f"'{p.name}' embeds; {', '.join(l)}")
-                jars[p.name] = l
+                print(f"'{n}' embeds; {', '.join(l)}")
+
+                if t:
+                    o[n] = l
+
+                return l
+    return None
+
+
+for p in Path(d).rglob("*.jar"):
+    # print(p)
+    recurse(str(p), p.name, jars, True)
 
 if jars:
     with open(Path(d) / "output.json", "w+") as f:
